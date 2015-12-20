9306d8673f9a90dceae40cc080f00a5a5adae9b5
Daniel Carneiro <daniel.carneiro@nmusic.pt>
2015-12-20 14:50:45 +0000

    load annotations while playing the video

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -24,21 +24,56 @@ let Video = {
       this.renderAnnotation(msgContainer, resp)
     })
 
+    msgContainer.addEventListener("click", e => {
+      e.preventDefault()
+      let seconds = e.target.getAttribute("data-seek")
+      if(!seconds){ return }
+
+      Player.seekTo(seconds)
+    })
+
     vidChannel.join()
-      .receive("ok", ({annotations}) => {
-        annotations.forEach( ann => this.renderAnnotation(msgContainer, ann))
+      .receive("ok", resp => {
+        this.scheduleMessages(msgContainer, resp.annotations)
       })
       .receive("error", reason => console.log("join failed", reason) )
-
   },
 
   renderAnnotation(msgContainer, {user, body, at}){
     let template = document.createElement("div")
     template.innerHTML = `
-    <b>${user.username}</b>: ${body}
+    <a href="#" data-seek="${at}">
+      [${this.formatTime(at)}] <b>${user.username}</b>: ${body}
+    </a>
     `
     msgContainer.appendChild(template)
     msgContainer.scrollTop = msgContainer.scrollHeight
+  },
+
+  scheduleMessages(msgContainer, annotations){
+    setTimeout(() => {
+      // adblock doesn't like this
+      let ctime = Player.getCurrentTime()
+      let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+      this.scheduleMessages(msgContainer, remaining)
+    }, 1000)
+  },
+
+  renderAtTime(annotations, seconds, msgContainer){
+    return annotations.filter( ann => {
+      if(ann.at > seconds){
+        return true
+      } else {
+        this.renderAnnotation(msgContainer, ann)
+        return false
+      }
+    })
+  },
+
+  formatTime(at){
+    let date = new Date(null)
+    date.setSeconds(at / 1000)
+    return date.toISOString().substr(14, 5)
   }
 }
 export default Video
