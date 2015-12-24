df6f5b288431a968e926e7ed55ebf503528e7e15
Daniel Carneiro <daniel.carneiro@nmusic.pt>
2015-12-24 11:31:58 +0000

    model tests - user password tests

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -14,4 +14,22 @@ defmodule Rumbl.UserTest do
     changeset = User.changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
   end
+
+  test "registration_changeset password must be at least 6 chars long" do
+    attrs = Map.put(@valid_attrs, :password, "no")
+    changeset = User.registration_changeset(%User{}, attrs)
+    refute changeset.valid?
+    assert {:password, {"should be at least %{count} characters", count: 6}}
+           in changeset.errors
+  end
+
+  test "registration_changeset with valid attributes hashes password" do
+    attrs = Map.put(@valid_attrs, :password, "superstrong")
+    changeset = User.registration_changeset(%User{}, attrs)
+    %{password: pass, password_hash: pass_hash} = changeset.changes
+
+    assert changeset.valid?
+    assert pass_hash
+    assert Comeonin.Bcrypt.checkpw(pass, pass_hash)
+  end
 end
